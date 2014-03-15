4cb835727a6deeb30a6271fcc4bb8ef6cf26f1a2
Miguel Alho <alho.miguelalho.com>
2014-03-15 22:42:02 +0000

    Added SquaringCalculatorTests class to test project; Added CanCalculateSquareOf unit test with 4 test cases.

---
+++ Calculators.Tests/SquaringCalculatorTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculators.Tests
+{
+    [TestFixture]
+    public class SquaringCalculatorTests
+    {
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(5, 25)]
+        [TestCase(-5, 25)]
+        public void CanCalculateSquareOf(int n, int expectedResult)
+        {
+            int result = SquareCalculator.Calculate(n);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
