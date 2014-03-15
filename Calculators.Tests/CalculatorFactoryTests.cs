e47fc5e04444dcc342207f453eb7e4a503fe17c7
Miguel Alho <alho.miguelalho.com>
2014-03-15 23:08:59 +0000

    Added CalculatorFactoryTests test fixture

---
+++ Calculators.Tests/CalculatorFactoryTests.cs
@@ -0,0 +1,23 @@
+﻿using Calculators.Interfaces;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculators.Tests
+{
+    [TestFixture]
+    public class CalculatorFactoryTests
+    {
+        [Test]
+        public void CanGetInstanceOfFibonacciCalculator()
+        {
+            IOrdinalCalculator calculator = CalculatorFactory.GetInstanceOf(CalculatorType.Fibonaci);
+
+            Assert.IsNotNull(calculator);
+            Assert.IsInstanceOf<FibonacciCalculator>(calculator);
+        }
+    }
+}
