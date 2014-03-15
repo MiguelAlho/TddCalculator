7ef5bb38b95cc492adcdb60320b3b2fd68a84e7c
Miguel Alho <alho.miguelalho.com>
2014-03-15 23:04:36 +0000

    Added CanCreateInstanceOfSquaringCalculator and CanCalculateSquareOf for instanced SquareCalculator Renamed CanCalculateForSquareOf for static Calculate Build fails

--- Calculators.Tests/SquaringCalculatorTests.cs
+++ Calculators.Tests/SquaringCalculatorTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Calculators.Interfaces;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,13 +11,36 @@ namespace Calculators.Tests
     [TestFixture]
     public class SquaringCalculatorTests
     {
+        [Test]
+        public void CanCreateInstanceOfSquaringCalculator()
+        {
+            var instance = new SquaringCalculator();
+
+            Assert.IsNotNull(instance);
+            Assert.IsInstanceOf<SquaringCalculator>(instance);
+            Assert.IsInstanceOf<IOrdinalCalculator>(instance);
+        }
+
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(5, 25)]
+        [TestCase(-5, 25)]
+        public void CanCalculateForSquareOf(int n, int expectedResult)
+        {
+            int result = SquaringCalculator.CalculateFor(n);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
         [TestCase(0, 0)]
         [TestCase(1, 1)]
         [TestCase(5, 25)]
         [TestCase(-5, 25)]
         public void CanCalculateSquareOf(int n, int expectedResult)
         {
-            int result = SquaringCalculator.CalculateFor(n);
+            IOrdinalCalculator calculator = new SquaringCalculator();
+
+            int result = calculator.Calculate(n);
 
             Assert.AreEqual(expectedResult, result);
         }
