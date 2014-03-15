7f2f441a2ec4e2dbf3b99daea81d97a4bf5b8904
Miguel Alho <alho.miguelalho.com>
2014-03-15 22:02:48 +0000

    Add Calculators Library project Added enough code for a completed build, but existing test fails. Test Project contains the reference to the production library

---
+++ Calculators/FibonacciCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculators
+{
+    public class FibonacciCalculator
+    {
+        public static int Calculate(int n)
+        {
+            throw new NotImplementedException("no logic for this yet");
+        }
+    }
+}
